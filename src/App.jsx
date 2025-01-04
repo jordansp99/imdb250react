498c147d71d57368a32a5d28ea94357bb80182e3
jordansp99 <jordanspeight@hotmail.com>
2025-01-04 00:16:44 +0000

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react'; // Import React!
+import './App.css';
+import Header from './Header'; // Correct import
 
 function App() {
   return (
-    <h1>hello</h1>
-  )
+    <div className="App"> {/* It's good practice to wrap your JSX in a single element */}
+      <Header /> {/* Use the Header component */}
+      <p>Content in App component</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
