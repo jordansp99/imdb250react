d5a032d13e8f0db55550a9d69649e88dab48f826
jordansp99 <jordanspeight@hotmail.com>
2025-01-04 20:49:31 +0000

    added form and MUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react'; // Import React!
 import './App.css';
-import Header from './Header'; // Correct import
+import Button from '@mui/material/Button';
 
 function App() {
   return (
-    <div className="App"> {/* It's good practice to wrap your JSX in a single element */}
-      <Header /> {/* Use the Header component */}
-      <p>Content in App component</p>
-    </div>
+    <>
+      
+    </>
   );
 }
 
