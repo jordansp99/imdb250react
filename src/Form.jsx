1aab96f752859f8910b72ed02794be7f88af6bb6
Jordan Speight <jordanspeight@hotmail.com>
2025-01-07 20:55:05 +0000

    added clue list

--- src/Form.jsx
+++ src/Form.jsx
@@ -3,17 +3,25 @@ import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import Card from '@mui/material/Card';
 import Button from '@mui/material/Button';
-import { Box, Container } from '@mui/material';
+import { Box, Container, Typography, List, ListItem, ListItemText } from '@mui/material';
 
 function Form() {
     const films = [
         { label: 'The Godfather', id: 1 },
         { label: 'Pulp Fiction', id: 2 },
     ];
+        const clues = ['Item 1', 'Item 2', 'Item 3', 'Item 4','item 5'];
     return (
-      <Container maxWidth="md" sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
+        <>
+        <Container maxWidth="md" sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
         <Card variant="outlined" sx={{ width: 500, p: 2 }}>
-            <h1>Guess the film</h1>
+        <List>
+            {clues.map((clue, index) => (
+        <ListItem key={index}>
+          <ListItemText primary={`${index + 1}. ${clue}`} />
+        </ListItem>
+      ))}
+        </List>
             <Box sx={{ display: 'flex', alignItems: 'center', gap: 2, mt:2 }}>
                 <Autocomplete
                     disablePortal
@@ -25,6 +33,7 @@ function Form() {
             </Box>
         </Card>
       </Container>
+      </>
     );
 }
 
