5e012867b0e1b1a92c5b3a83079a5b7ce832a16a
jordansp99 <jordanspeight@hotmail.com>
2025-01-07 20:19:01 +0000

    added dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,60 @@
 import React from 'react';
 import Form from './Form';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { deepPurple } from '@mui/material/colors';
-import CssBaseline from '@mui/material/CssBaseline'; // Import CssBaseline
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: deepPurple[500],
-    },
-    secondary: {
-      main: deepPurple[100],
-    },
-  },
-  typography: {
-    fontFamily: 'Roboto, sans-serif', // Set your desired font family
-    // Example of customizing other typography variants
-    h1: {
-      fontWeight: 500,
-    },
-    body1: {
-      fontSize: '1rem', // Example font size
-    }
-  },
-});
+import { yellow } from '@mui/material/colors';
+import CssBaseline from '@mui/material/CssBaseline';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 function App() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = React.useMemo( //prevents unnecesary rerenders
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light', // Set mode based on user preference
+          ...(prefersDarkMode // Conditional palette settings
+            ? {
+                // palette values for dark mode
+                primary: yellow,
+                background: {
+                  default: '#121212', // Dark background
+                  paper: '#1e1e1e', // Darker background for paper elements
+                },
+                text: {
+                  primary: '#fff', // White text
+                  secondary: '#aaa', // Slightly lighter text
+                },
+              }
+            : {
+                // palette values for light mode
+                primary: yellow,
+                background: {
+                  default: '#fff', // White background
+                  paper: '#fff',
+                },
+                text: {
+                  primary: '#000', // Black text
+                  secondary: '#777',
+                },
+              }),
+        },
+        typography: {
+          fontFamily: 'Roboto, sans-serif',
+          h1: {
+            fontWeight: 500,
+          },
+          body1: {
+            fontSize: '1rem',
+          },
+        },
+      }),
+    [prefersDarkMode],
+  );
+
   return (
     <ThemeProvider theme={theme}>
-      {/* CssBaseline should be inside the ThemeProvider */}
-      <CssBaseline /> 
+      <CssBaseline />
       <Form />
     </ThemeProvider>
   );
