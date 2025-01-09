0f9ce88752c890f9be051249c6707f1d8823a3f4
Jordan Speight <jordanspeight@hotmail.com>
2025-01-09 19:16:18 +0000

    imported jsonl

--- src/Form.jsx
+++ src/Form.jsx
@@ -5,18 +5,17 @@ import Card from '@mui/material/Card';
 import Button from '@mui/material/Button';
 import { Box, Container, Typography, List, ListItem, ListItemText, Tooltip, IconButton } from '@mui/material';
 import InfoIcon from '@mui/icons-material/Info';
+import filmsData from './clues_with_date.jsonl'; // Import the JSONL data
 
 function Form() {
-    const [filmsData, setFilmsData] = useState([]);
     const [todaysFilm, setTodaysFilm] = useState(null);
     const [selectedFilm, setSelectedFilm] = useState(null);
     const [currentClueIndex, setCurrentClueIndex] = useState(0);
     const [clues, setClues] = useState([]);
     const [showSubmit, setShowSubmit] = useState(true);
-     const [answerYear, setAnswerYear] = useState(null);
+    const [answerYear, setAnswerYear] = useState(null);
     const [revealAnswer, setRevealAnswer] = useState(false);
-
-
+    
     // Helper function for seeded random number generation
     const seededRandom = (seed) => {
         const x = Math.sin(seed) * 10000;
@@ -28,27 +27,8 @@ function Form() {
         const today = new Date();
         const seed = today.getFullYear() * 10000 + today.getMonth() * 100 + today.getDate();
         const randomIndex = Math.floor(seededRandom(seed) * filmsData.length);
-
         return randomIndex
     };
-
-    // Fetch and process JSONL data
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                 const response = await fetch('./clues_with_date.jsonl');
-                 const text = await response.text();
-                 const lines = text.trim().split('\n');
-                 const parsedData = lines.map(line => JSON.parse(line));
-                 setFilmsData(parsedData);
-             } catch (error) {
-                 console.error("Error fetching or parsing clues.json:", error);
-             }
-        };
-
-        fetchData();
-     }, []);
-
 
     // Set todays film when filmsData is populated
     useEffect(() => {
@@ -57,16 +37,16 @@ function Form() {
            const todaysFilm = filmsData[todaysFilmIndex];
            setTodaysFilm(todaysFilm);
            setClues([todaysFilm.Clues[0]]); // Set initial clue
-            setAnswerYear(todaysFilm.Year); // Set the answer year
+           setAnswerYear(todaysFilm.Year); // Set the answer year
         }
-    }, [filmsData]);
+    }, []);
 
 
     // Process the films data to extract labels and IDs
-     const films = filmsData.map((film, index) => ({
-         label: film['Film Name'],
-         id: index + 1,
-     }));
+    const films = filmsData.map((film, index) => ({
+        label: film['Film Name'],
+        id: index + 1,
+    }));
 
     // Function to extract clues from the JSON
     const getClues = (filmName) => {
@@ -88,20 +68,20 @@ function Form() {
             setClues(allClues);
             setShowSubmit(false);
             setRevealAnswer(true);
-             alert("Correct!");
+            alert("Correct!");
         } else {
             if (currentClueIndex < allClues.length - 1 ) {
-                 setClues(prevClues => [...prevClues, allClues[currentClueIndex + 1]]);
-                 setCurrentClueIndex(prevIndex => prevIndex + 1);
-                 setSelectedFilm(null);
-            }
+                setClues(prevClues => [...prevClues, allClues[currentClueIndex + 1]]);
+                setCurrentClueIndex(prevIndex => prevIndex + 1);
+                setSelectedFilm(null);
+           }
             else {
-                 alert(`You have exhausted all the clues, the answer was ${todaysFilm['Film Name']}.`);
-                 setSelectedFilm(null);
-                 setClues([getClues(todaysFilm['Film Name'])[0]]); // Reset the clues
-                 setCurrentClueIndex(0);
-                setShowSubmit(true);
-                 setRevealAnswer(false);
+                alert(`You have exhausted all the clues, the answer was ${todaysFilm['Film Name']}.`);
+                setSelectedFilm(null);
+                setClues([getClues(todaysFilm['Film Name'])[0]]); // Reset the clues
+                setCurrentClueIndex(0);
+               setShowSubmit(true);
+                setRevealAnswer(false);
             }
         }
     };
